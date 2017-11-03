e3a6b8567285f90a587cb28fbce5dce69b290972
Fernando Alves <up201605270@fe.up.pt>
2017-11-03 15:31:55 +0000

    Small error corrections

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,16 @@
  *  Created on: 31/10/2017
  *      Author: tripor
  */
-
+#include "Header.h"
 #include <iostream>
 using namespace std;
 int main()
 {
-	cout << "Ola Mundo";
-	return 0;
+	/*
+	std::pair<int,int> p1 = make_pair(1,3);
+	std::cout << p1.first << '\n';
+	std::cout << p1.second << '\n';
+	*/
 }
 
 
