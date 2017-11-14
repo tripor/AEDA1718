d6efdff67f1f021448747aab36cdc1c177626548
Fernando Alves <up201605270@fe.up.pt>
2017-11-14 18:22:06 +0000

    bug fixes

--- src/main.cpp
+++ src/main.cpp
@@ -26,3 +26,52 @@ int main()
 }
 
 
+/*
+
+
+ 	std::string opcao;
+
+	std::cout << "Indique o tipo de acidente: \n";
+	std::cout << "1 - Incendio Florestal\n";
+	std::cout << "2 - Incendio Domestico\n";
+	std::cout << "3 - Assalto\n";
+	std::cout << "4 - Acidente de Viacao\n";
+	std::cout << "0 - Voltar ao Menu Inicial\n";
+
+	while (!(opcao == "1" || opcao == "2" || opcao == "3" || opcao == "4" || opcao == "0"))
+		{
+			std::cout << "\n(!) A opcao fornecida nao e valida (!)\n\n";
+			std::cout << "Indique o tipo de acidente: ";
+			std::cin >> opcao;
+		}
+
+	Acidente *a;
+
+	if (opcao == "1")
+	{
+		a = new Florestal;
+	}
+	if (opcao == "2")
+	{
+		a = new Domesticos;
+	}
+	if (opcao == "3")
+	{
+		a = new Assalto;
+	}
+	if (opcao == "4")
+	{
+		a = new Acidente_Viacao;
+	}
+	if (opcao == "0")
+	{
+		return;
+	}
+	//falta a atribuicao, fazer por pointer?
+	//falta testar se acidente já existe
+	this->acidentes.push_back(a);
+
+
+
+ */
+
