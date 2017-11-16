7eed5c7f9aa4efedc17f8f732c6265a38dd3b2e3
MiguelFerreira1998 <33258304+MiguelFerreira1998@users.noreply.github.com>
2017-11-16 17:43:43 +0000

    PrintAcidentes
    print

--- src/Miguel.cpp
+++ src/Miguel.cpp
@@ -1,4 +1,4 @@
-/*#include "Header.h"
+#include "Header.h"
 #include "PostoSocorro.h"
 #include "Acidente.h"
 #include "Menu.h"
@@ -6,6 +6,101 @@
 //////// PARTE A COPIAR
 
 
+
+void Menu::printAcidentes(){
+
+		std::stringstream ss;
+	for(unsigned int i = 0; i<acidentes.size(); i++){
+
+		std::cout << "<" << acidentes.at(i)->getTipoAcidente()<< std::endl;
+		std::cout << "<" << "[" << acidentes.at(i)->getLocal().first<<",";
+		std::cout <<acidentes.at(i)->getLocal().second <<"]"<<std::endl;
+		(acidentes.at(i)->getData()).printDate();
+		std::cout << std::endl;
+		std::string a = acidentes.at(i)->getTipoAcidente();//para as condições
+
+		ss << acidentes.at(i)->getAllInfo();
+
+		if(a=="Incendio_Florestal"){
+
+			std::string n_carros;
+			ss >> n_carros;
+			std::cout<<"Numero de carros de bombeiros: "<<n_carros<<std::endl;
+
+			std::string n_bombeiros;
+			ss >> n_bombeiros;
+			std::cout<<"Numero de bombeiros: "<<n_bombeiros<<std::endl;
+
+			std::string area_chamas;
+			ss >> area_chamas;
+			std::cout<<"area em chamas: "<<area_chamas<<std::endl;
+		}
+
+		else if (a=="Incendio_Domestico"){
+			std::string n_carros;
+			ss >> n_carros;
+			std::cout<<"Numero de carros de bombeiros: "<<n_carros<<std::endl;
+
+			std::string n_bombeiros;
+			ss >> n_bombeiros;
+			std::cout<<"Numero de bombeiros: "<<n_bombeiros<<std::endl;
+
+			std::string tipo_casa;
+			ss>>tipo_casa;
+			std::cout<<"Tipo de casa: "<<tipo_casa<<std::endl;
+
+		}
+
+		else if(a=="Assalto"){
+			std::string feridos;
+			ss>>feridos;
+			if(feridos=="0")
+				std::cout<<"Nao existem feridos"<<std::endl;
+			else
+				std::cout<<"Existem feridos"<<std::endl;
+
+			std::string tipo_casa;
+			ss>>tipo_casa;
+			std::cout<<"Tipo de casa: " << tipo_casa << std::endl;
+
+
+		}
+
+
+		else if(a=="AcidenteViacao"){
+
+			std::string n_feridosGraves;
+			ss>>n_feridosGraves;
+			std::cout<<"Numero de feridos graves: " << n_feridosGraves << std::endl;
+
+			std::string n_veiculos;
+			ss>>n_veiculos;
+			std::cout<<"Numero de veiculos envolvidos: " << n_veiculos << std::endl;
+
+			std::string tipo_estrada;
+			ss>>tipo_estrada;
+			std::cout<<"Tipo de estrada: " << tipo_estrada << std::endl;
+
+
+		}
+	}
+}
+
+void Menu::printPostos(){
+
+		std::stringstream ss;
+	for(unsigned int i = 0; i<postos_socorro.size(); i++){
+
+		std::cout << "< Numero de socorristas" << postos_socorro.at(i)->getSocorristas()<< std::endl;
+		std::cout << "< Numero de veiculos" << postos_socorro.at(i)->getVeiculos()<< std::endl;
+		std::cout << "<" << "[" << postos_socorro.at(i)->getPos().first<<",";
+		std::cout <<postos_socorro.at(i)->getPos().second <<"]"<<std::endl;
+
+
+	}
+}
+
+/*
 
 std::string Florestal::WriteAcidente(){
 	std::stringstream ss;
@@ -79,4 +174,5 @@ void EscreveFicheiroAcidente(){
 	}
 		  ficheiro.close();
 }
+
 */
